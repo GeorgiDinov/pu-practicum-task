e48b831cc4f37fd9b82f4147229dbdb378dc44e3
Georgi Dinov <georgidinov1@abv.bg>
2020-11-27 11:25:03 +0200

    Added multiple mapping to the home controller get mapping.

--- src/main/java/com/practicum/pu/georgidinov/shoppinglist/controller/HomePageController.java
+++ src/main/java/com/practicum/pu/georgidinov/shoppinglist/controller/HomePageController.java
@@ -1,11 +1,25 @@
 package com.practicum.pu.georgidinov.shoppinglist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.HOME_PAGE_MAPPING_DEFAULT;
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.HOME_PAGE_MAPPING_HOME;
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.HOME_PAGE_MAPPING_HOME_HTML;
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.HOME_PAGE_MAPPING_INDEX;
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.HOME_PAGE_MAPPING_INDEX_HTML;
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.HOME_PAGE_MAPPING_SLASH;
+import static com.practicum.pu.georgidinov.shoppinglist.util.ApplicationConstants.VIEW_NAME_HOME_PAGE;
+
 
 @Controller
 public class HomePageController {
 
-
-
+    @GetMapping({HOME_PAGE_MAPPING_DEFAULT, HOME_PAGE_MAPPING_SLASH,
+            HOME_PAGE_MAPPING_HOME, HOME_PAGE_MAPPING_HOME_HTML,
+            HOME_PAGE_MAPPING_INDEX, HOME_PAGE_MAPPING_INDEX_HTML})
+    public String getAllItems() {
+        return VIEW_NAME_HOME_PAGE;
+    }
 
 }
