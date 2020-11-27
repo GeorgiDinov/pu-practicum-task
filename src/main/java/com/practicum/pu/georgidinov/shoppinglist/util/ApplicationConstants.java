2020a5a1bf9cb63c2e63a0c5647c286215bb42d0
Georgi Dinov <georgidinov1@abv.bg>
2020-11-27 11:19:15 +0200

    Added constants global to the application.

---
+++ src/main/java/com/practicum/pu/georgidinov/shoppinglist/util/ApplicationConstants.java
@@ -0,0 +1,25 @@
+package com.practicum.pu.georgidinov.shoppinglist.util;
+
+public final class ApplicationConstants {
+
+    //== constructor ==
+    private ApplicationConstants() {
+    }
+
+    //== View Names ==
+    public static final String VIEW_NAME_HOME_PAGE = "home";
+
+    //== Mapping Names ==
+    public static final String HOME_PAGE_MAPPING_DEFAULT = "";
+    public static final String HOME_PAGE_MAPPING_SLASH = "/";
+    public static final String HOME_PAGE_MAPPING_HOME = "home";
+    public static final String HOME_PAGE_MAPPING_HOME_HTML = "home.html";
+    public static final String HOME_PAGE_MAPPING_INDEX = "index";
+    public static final String HOME_PAGE_MAPPING_INDEX_HTML = "index.html";
+
+    //== Error Messages ==
+    public static final String VALIDATION_ERROR_ENTITY_IS_NULL = "Entity Validation Error Entity Is Null";
+    public static final String VALIDATION_ERROR_ENTITY_ID_IS_NULL = "Entity Validation Error Entity Id Is Null";
+    public static final String VALIDATION_ERROR_ENTITY_NAME_IS_NULL = "Entity Validation Error Entity Name Is Null";
+
+}
