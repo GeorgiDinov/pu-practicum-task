ac35719c39b241e1fbe6d44b8a2aa3b2505535a8
Georgi Dinov <georgidinov1@abv.bg>
2021-02-02 15:56:11 +0200

    Refactored react client folder

--- src/main/java/com/practicum/pu/georgidinov/shoppinglist/security/SecurityConfig.java
+++ src/main/java/com/practicum/pu/georgidinov/shoppinglist/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(new JwtUsernamePasswordAuthenticationFilter(authenticationManager(), this.jwtPropertyHolder))
                 .addFilterAfter(new JwtTokenVerifier(this.jwtPropertyHolder), JwtUsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/css/*", "/js/*", "/h2-console/**", "/", "/register").permitAll()
+                .antMatchers("/", "/register").permitAll()
                 .antMatchers("/items/**").hasRole(USER.name())
                 .anyRequest()
                 .authenticated();
